86b1dddee63d2b7b44f612f1aa6db0e38f5a844b
souravdev-eng <105838250+souravmusquaretech@users.noreply.github.com>
2022-10-26 08:07:40 +0530

    complete section 2

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish('count', {
-          count,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator('count');
-    },
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const post = db.posts.find((post) => post.id === postId && post.published);
@@ -26,7 +12,7 @@ const Subscription = {
   },
   post: {
     subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator('newPost');
+      return pubsub.asyncIterator('post');
     },
   },
 };
